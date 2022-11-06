4df20d6ca0acf597c1df5ec60c3d8d4bba4e75ab
koffe0522 <kouhei19920522@gmail.com>
2022-11-07 00:00:56 +0900

    必要？

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,17 +18,17 @@ task("accounts", "Prints the list of accounts", accounts);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
-  defaultNetwork: "goerli",
+  // defaultNetwork: "goerli",
     networks: {
     hardhat: {},
-    goerli: {
-      url: process.env.API_URL,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-    },
+    // goerli: {
+    //   url: process.env.API_URL,
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    // },
   },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // },
 };
 
 export default config;
